45fc6133025fe0fce9cbab4feccdb91caa2ac936
nhivuanh <nhi.vu@opswat.com>
2022-09-13 14:09:50 +0700

    Update solution after reviewed

--- Main-TwosComplement/Main-TwosComplement/Main-TwosComplement.cpp
+++ Main-TwosComplement/Main-TwosComplement/Main-TwosComplement.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <bitset>
 
+void twosComplement(unsigned char &binary)
+{
+    bool flip = false;
+
+    for (int j = 0; j < 8; j++)
+    {
+        if (flip)
+            binary ^= (1 << j);
+        if (binary & (1 << j))
+            flip = true;
+    }
+}
+
 int main()
 {
     unsigned char arr[10] = { 0, 1, 2, 126, 127, 128, 129, 130, 254, 255 };
 
-    // Invert all bits
+    // Calculate 
     for (int i = 0; i < 10; i++)
-        for (int j = 0; j < 8; j++)
-            arr[i] ^= (1 << j);
-
-    // Add one 
-    for (int i = 0; i < 10; i++)
-    {
-        bool carry = 1;
-        for (int j = 0; j < 8; j++)
-        {
-            if (carry == 1)
-            {
-                // if 0 + carry(1) = 1 -> carry = 0 
-                if ((arr[i] & (1 << j)) == 0)
-                    carry = 0;
-                arr[i] ^= (1 << j);
-            }
-        }
-    }
+        twosComplement(arr[i]);
 
     // For printing the results only
     for (int i = 0; i < 10; i++)
