2667b0d4250bb896313da0e46426a4d8f4bc5af7
nhivuanh <nhi.vu@opswat.com>
2022-09-13 09:28:32 +0700

    minor change

--- Main-TwosComplement/Main-TwosComplement/Main-TwosComplement.cpp
+++ Main-TwosComplement/Main-TwosComplement/Main-TwosComplement.cpp
@@ -7,12 +7,8 @@ int main()
 
     // Invert all bits
     for (int i = 0; i < 10; i++)
-    {
         for (int j = 0; j < 8; j++)
-        {
             arr[i] ^= (1 << j);
-        }
-    }
 
     // Add one 
     for (int i = 0; i < 10; i++)
