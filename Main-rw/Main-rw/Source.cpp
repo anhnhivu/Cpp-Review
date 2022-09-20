6e52edbc58da043eb9cb81313ef11887ca78f87c
anhnhivu <anhnhi160801@gmail.com>
2022-09-20 20:36:02 +0700

    Use lock_guard

--- Main-rw/Main-rw/Source.cpp
+++ Main-rw/Main-rw/Source.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <semaphore>
 #include <thread>
+#include <shared_mutex>
 
+std::shared_mutex mutex;
 int numberOfSeats = 100;
 
-std::counting_semaphore<1> readwriteSignal(0);
-std::counting_semaphore<1> writeSignal(0);
-
-void reserveASeat() 
+void readNumberOfSeats(int& numberOfSeatsToRead)
 {
-    std::cout << "Writer: Waiting to write." << '\n';
+    std::shared_lock<std::shared_mutex> lock(mutex);
+    numberOfSeatsToRead = numberOfSeats;
+}
 
+void reserveASeat()
+{
+    std::lock_guard<std::shared_mutex> lock(mutex);
     numberOfSeats--;
-    std::cout << "Reserve a seat, remaining: " << numberOfSeats << '\n';
-
-    readwriteSignal.release();
 }
 
-void readNumberOfSeats() 
-{
-    std::cout << "Reader: Waiting to read." << '\n';
-    readwriteSignal.acquire();
+int main() {
+    int read1;
+    int read2;
+    int read3;
+    int read4;
+    int read5;
+    int read6;
 
-    std::cout << "Number of seats available: " << numberOfSeats << '\n';
-    std::cout << '\n';
-
-}
-
-int main() 
-{
-    std::thread t1(reserveASeat);               
-    std::thread t2(readNumberOfSeats);          // 99
+    std::thread t1(readNumberOfSeats, std::ref(read1));
+    std::thread t2(readNumberOfSeats, std::ref(read2));
     std::thread t3(reserveASeat);
-    std::thread t4(readNumberOfSeats);          // 98
+    std::thread t4(readNumberOfSeats, std::ref(read3));
     std::thread t5(reserveASeat);
-    std::thread t6(reserveASeat);
-    std::thread t7(readNumberOfSeats);          // 96
-    std::thread t8(readNumberOfSeats);          // 96
+    std::thread t6(readNumberOfSeats, std::ref(read4));
+    std::thread t7(reserveASeat);
+    std::thread t8(readNumberOfSeats, std::ref(read5));
+    std::thread t9(reserveASeat);
+    std::thread t10(readNumberOfSeats, std::ref(read6));
+    std::thread t11(reserveASeat);
+    std::thread t12(reserveASeat);
 
     t1.join();
     t2.join();
@@ -46,7 +46,16 @@ int main()
     t6.join();
     t7.join();
     t8.join();
+    t9.join();
+    t10.join();
+    t11.join();
+    t12.join();
 
-    std::cout << '\n';
-
+    std::cout << read1 << "\n"; // 100
+    std::cout << read2 << "\n"; // 100
+    std::cout << read3 << "\n";
+    std::cout << read4 << "\n";
+    std::cout << read5 << "\n";
+    std::cout << read6 << "\n";
+    std::cout << numberOfSeats << "\n";
 }
